9a5c35b8e38ba05176524901a5057372e70e6c7a
Kenneth Skelton <kskelto1@stevens.edu>
2021-09-09 10:31:50 -0400

    Initial Commit

---
+++ CookGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookGuide
+//
+//  Created by Kenneth Skelton on 9/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookGuide/CookGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookGuideApp.swift
+//  CookGuide
+//
+//  Created by Kenneth Skelton on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CookGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
