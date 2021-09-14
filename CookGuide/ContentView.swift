4da966c354f63318a48326e97da25c215ac60805
Kenneth Skelton <kskelto1@stevens.edu>
2021-09-14 16:54:19 -0400

    Started the recipe creator, and created a file to handle serving the views information

--- CookGuide/ContentView.swift
+++ CookGuide/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
+    @State var existingRecipes = createTestData()
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            HStack{
+                
+            Text("My Recipes")
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Button(
+                    action: {},
+                    label: {
+                        Text("+").font(.system(size:60))
+                    }
+                )
+                    .buttonStyle(BorderlessButtonStyle())
+                    .frame(width: 50, height: 50)
+                    .border(Color.black)
+                    
+                
+
+            
+            }
+            Spacer()
+            List(){
+                ForEach(0..<existingRecipes.count, id: \.self){
+                    idx in Button(
+                        action: {
+                            
+                        },
+                        label:{
+                            HStack{
+                                Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+                                Text(existingRecipes[idx])
+                                
+                            }
+                        }
+                    )
+                }
+            }
+            
+        }
+        
     }
 }
 
---
+++ CookGuide/Recipe View Initializer.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe View Initializer.swift
+//  CookGuide
+//
+//  Created by Kenneth Skelton on 9/14/21.
+//
+
+import Foundation
+
+func createTestData() -> Array<String>{
+    let testNames = ["eggs", "milk", "cheese", "bacon", "soup", "lettuce", "fish", "beans", "crab", "hamburgers","eggs", "milk", "cheese", "bacon", "soup", "lettuce", "fish", "beans", "crab", "hamburgers"]
+    
+    return testNames
+}
