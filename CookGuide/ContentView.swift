d16d2ae32011d25ea25d6b76d98ea1bcf86420de
Kenneth Skelton <kskelto1@stevens.edu>
2021-09-16 14:11:33 -0400

    Added functionality to add ingredients to recipes, and switch between the ingredient screen and the normal one

--- CookGuide/ContentView.swift
+++ CookGuide/ContentView.swift
@@ -16,46 +16,45 @@ struct ContentView: View {
     
     
     var body: some View {
-        
-        VStack{
-            HStack{
-                
-            Text("My Recipes")
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                Button(
-                    action: {},
-                    label: {
-                        Text("+").font(.system(size:60))
-                    }
-                )
-                    .buttonStyle(BorderlessButtonStyle())
-                    .frame(width: 50, height: 50)
-                    .border(Color.black)
+        NavigationView{
+            VStack{
+                HStack{
                     
-                
-
-            
-            }
-            Spacer()
-            List(){
-                ForEach(0..<existingRecipes.count, id: \.self){
-                    idx in Button(
-                        action: {
-                            
-                        },
-                        label:{
-                            HStack{
-                                Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
-                                Text(existingRecipes[idx])
-                                
-                            }
+                Text("My Recipes")
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                        NavigationLink(destination: AddIngredientView()){
+                            Text("+").font(.system(size:60))
+                                .frame(width: 50, height: 50)
+                                .border(Color.black)
                         }
-                    )
+                    
+                        
+                        
+                    
+
+                
                 }
+                Spacer()
+                List(){
+                    ForEach(0..<existingRecipes.count, id: \.self){
+                        idx in Button(
+                            action: {
+                                
+                            },
+                            label:{
+                                HStack{
+                                    Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+                                    Text(existingRecipes[idx])
+                                    
+                                }
+                            }
+                        )
+                    }
+                }
+                Spacer()
             }
-            
         }
-        
     }
 }
 
